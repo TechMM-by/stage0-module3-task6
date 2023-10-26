aa7ededb2b4e3a132decff747923e1d0f62166fb
Maria Maksimova <drogzed@gmail.com>
2023-10-26 21:58:49 +0400

    first

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        double result = Math.round(numberToBeRounded);
+        int finish;
+        finish = (int) result;
+
+        System.out.println(finish);
+
+
     }
 }
